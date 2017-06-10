a6aae6b0440efc1c6d1320d76ddca31c5d770aec
tommy <940987456@qq.com>
2017-06-11 00:18:35 +0800

    add static files

---
+++ js/index_data.js
@@ -0,0 +1,160 @@
+var portal_url = '/';
+var c_data = c_data || {};
+c_data.animations = [{
+    "iType": "ani",
+    "element_id": "div990aeaee",
+    "data": {
+        "type": 1,
+        "t": {"rv": 0, "rp": 0, "name": "", "wa": 0, "de": 0, "st": 2, "du": 0.5, "type": 1, "es": 0},
+        "d": {"dl": 30, "dt": 1, "di": 3}
+    },
+    "id": "ani_60d48ce9"
+}, {
+    "iType": "ani",
+    "element_id": "heading1f535964",
+    "data": {
+        "type": 3,
+        "t": {"rv": 0, "rp": 0, "name": "", "wa": 0, "de": 0, "st": 1, "du": 1, "type": 3, "es": 0},
+        "d": {}
+    },
+    "id": "ani_16b2f624"
+}];
+c_data.timelines = [{
+    "iType": 0,
+    "element_id": "section73ef9f42",
+    "data": {"type": 0, "t": {"rv": 0, "rp": 0, "wa": 0, "de": 0, "st": 1, "du": 1, "es": 0}, "d": {}},
+    "id": "tl_f83f28f0",
+    "animations": []
+}, {
+    "iType": 0,
+    "element_id": "body_39a3c694f183d488",
+    "data": {"type": 0, "t": {"rv": 0, "rp": 0, "wa": 0, "de": 0, "st": 1, "du": 1, "es": 0}, "d": {}},
+    "id": "M_d131f8b279be4f1a",
+    "animations": ["ani_1fddada7", "ani_60d48ce9"]
+}, {
+    "iType": 0,
+    "element_id": "sectiond18c10cd",
+    "data": {"type": 0, "t": {"rv": 0, "rp": 0, "wa": 0, "de": 0, "st": 1, "du": 1, "es": 0}, "d": {}},
+    "id": "tl_b21c2499",
+    "animations": []
+}, {
+    "iType": 0,
+    "element_id": "sectionf4de844b",
+    "data": {"type": 0, "t": {"rv": 0, "rp": 0, "wa": 0, "de": 0, "st": 1, "du": 1, "es": 0}, "d": {}},
+    "id": "tl_e3f4fbe3",
+    "animations": []
+}, {
+    "iType": 0,
+    "element_id": "section85dcc372",
+    "data": {"type": 0, "t": {"rv": 0, "rp": 0, "wa": 0, "de": 0, "st": 1, "du": 1, "es": 0}, "d": {}},
+    "id": "tl_3f65caf5",
+    "animations": []
+}, {
+    "iType": 0,
+    "element_id": "section4378f9be",
+    "data": {"type": 0, "t": {"de": 0, "rp": 0, "name": "", "st": 2}, "d": {}},
+    "id": "tl_47b6cdf7",
+    "animations": ["ani_b5271a8859549a49", "ani_16b2f624", "ani_a2e1d6b7de32fd93"]
+}, {
+    "iType": 0,
+    "element_id": "sectione8a2debe",
+    "data": {"type": 0, "t": {"rv": 0, "rp": 0, "wa": 0, "de": 0, "st": 1, "du": 1, "es": 0}, "d": {}},
+    "id": "tl_d26a9066",
+    "animations": []
+}, {
+    "iType": 0,
+    "element_id": "sectiond6f5e9a3",
+    "data": {"type": 0, "t": {"rv": 0, "rp": 0, "wa": 0, "de": 0, "st": 1, "du": 1, "es": 0}, "d": {}},
+    "id": "tl_efeebe13",
+    "animations": []
+}, {
+    "iType": 0,
+    "element_id": "sectiond5fb679e",
+    "data": {"type": 0, "t": {"rv": 0, "rp": 0, "wa": 0, "de": 0, "st": 1, "du": 1, "es": 0}, "d": {}},
+    "id": "tl_fd6c9231",
+    "animations": []
+}, {
+    "iType": 0,
+    "element_id": "section976bdec6",
+    "data": {"type": 0, "t": {"rv": 0, "rp": 0, "wa": 0, "de": 0, "st": 1, "du": 1, "es": 0}, "d": {}},
+    "id": "tl_0ed18106",
+    "animations": []
+}, {
+    "iType": 0,
+    "element_id": "section73ed9d84",
+    "data": {"type": 0, "t": {"rv": 0, "rp": 0, "wa": 0, "de": 0, "st": 1, "du": 1, "es": 0}, "d": {}},
+    "id": "tl_58e1be22",
+    "animations": []
+}, {
+    "iType": 0,
+    "element_id": "sectiondf0b8fd2",
+    "data": {"type": 0, "t": {"rv": 0, "rp": 0, "wa": 0, "de": 0, "st": 1, "du": 1, "es": 0}, "d": {}},
+    "id": "tl_072db10b",
+    "animations": []
+}, {
+    "iType": 0,
+    "element_id": "section8befdd83",
+    "data": {"type": 0, "t": {"rv": 0, "rp": 0, "wa": 0, "de": 0, "st": 1, "du": 1, "es": 0}, "d": {}},
+    "id": "tl_31c5cdb5",
+    "animations": []
+}, {
+    "iType": 0,
+    "element_id": "section9f8b8528",
+    "data": {"type": 0, "t": {"rv": 0, "rp": 0, "wa": 0, "de": 0, "st": 1, "du": 1, "es": 0}, "d": {}},
+    "id": "tl_3c5eba89",
+    "animations": []
+}, {
+    "iType": 0,
+    "element_id": "section6bf337e8",
+    "data": {"type": 0, "t": {"rv": 0, "rp": 0, "wa": 0, "de": 0, "st": 1, "du": 1, "es": 0}, "d": {}},
+    "id": "tl_7f900b2c",
+    "animations": []
+}, {
+    "iType": 0,
+    "element_id": "sectionb64f1146",
+    "data": {"type": 0, "t": {"rv": 0, "rp": 0, "wa": 0, "de": 0, "st": 1, "du": 1, "es": 0}, "d": {}},
+    "id": "tl_4e8af143",
+    "animations": []
+}, {
+    "iType": 0,
+    "element_id": "section1b592cb4",
+    "data": {"type": 0, "t": {"rv": 0, "rp": 0, "wa": 0, "de": 0, "st": 1, "du": 1, "es": 0}, "d": {}},
+    "id": "tl_290ecc4c",
+    "animations": []
+}, {
+    "iType": 0,
+    "element_id": "sectionadc7e84d",
+    "data": {"type": 0, "t": {"rv": 0, "rp": 0, "wa": 0, "de": 0, "st": 1, "du": 1, "es": 0}, "d": {}},
+    "id": "tl_75fd493c",
+    "animations": []
+}, {
+    "iType": 0,
+    "element_id": "sectionc4ae6fb2",
+    "data": {"type": 0, "t": {"rv": 0, "rp": 0, "wa": 0, "de": 0, "st": 1, "du": 1, "es": 0}, "d": {}},
+    "id": "tl_ca57ba44",
+    "animations": []
+}, {
+    "iType": 0,
+    "element_id": "sectione24d237c",
+    "data": {"type": 0, "t": {"rv": 0, "rp": 0, "wa": 0, "de": 0, "st": 1, "du": 1, "es": 0}, "d": {}},
+    "id": "tl_1e492b23",
+    "animations": []
+}, {
+    "iType": 0,
+    "element_id": "section9d3ad19e",
+    "data": {"type": 0, "t": {"rv": 0, "rp": 0, "wa": 0, "de": 0, "st": 1, "du": 1, "es": 0}, "d": {}},
+    "id": "tl_3c141c0c",
+    "animations": []
+}];
+c_data.actions = [{
+    "element_id": "linkblock1e40a0f4",
+    "data": {
+        "args": {
+            "e_ids": ["icon1fac4612", "iconaf5904a3", "navcollapseb9aaba37"],
+            "cla": "c-state1",
+            "st": 1,
+            "a_ids": []
+        }, "type": 0, "exec": 23
+    },
+    "id": "act_eb360d5c"
+}]
